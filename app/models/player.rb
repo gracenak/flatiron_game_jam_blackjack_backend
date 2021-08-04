c2b0267f23bd54c3a85c4f4e4aa1e8587ddbcad5
Sean <Sransonette@gmail.com>
2021-08-03 22:29:44 -0500

    tables needed to be plural so we deleted and recreated tables for deck and player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
     
+
 end
--- db/migrate/20210804024510_deck.rb
+++
@@ -1,8 +0,0 @@
-class Deck < ActiveRecord::Migration[6.0]
-  def change
-    create_table :deck do |t|
-      t.string :value
-      t.string :suit
-    end
-  end
-end
