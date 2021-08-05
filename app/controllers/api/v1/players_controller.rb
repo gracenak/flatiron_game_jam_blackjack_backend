c3141682e47b11e62c841c492941bcdc61c8dadc
JakeKViti <thevitij64@gmail.com>
2021-08-04 21:46:52 -0400

    Built Out PlayersControllers

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,18 +1,25 @@
 class Api::V1::PlayersController < ApplicationController
 
     def index
-
+        player = Player.all
+        render json: player
     end
 
     def show
-
+        player = Player.find_by(id: params[:id])
     end
 
-    def destroy
-
+    def create
+        player = Player.create(player_params)
+        render json: player
     end
 
-    
+    private
 
+    def player_params
+        params.require(:players).permit(
+            :hand,
+            :hand_value
+        )
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 player = Player.create(hand: 'King', hand_value: 10)
+player1 = Player.create(hand: 'Two', hand_value: 2)
+player2 = Player.create(hand: 'Seven', hand_value: 7)
+player3 = Player.create(hand: 'Ten', hand_value: 10)
