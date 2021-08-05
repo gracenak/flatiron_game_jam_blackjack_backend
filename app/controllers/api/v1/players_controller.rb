e5d11c3e25c47328492426011a3d50e40d83ca15
JakeKViti <thevitij64@gmail.com>
2021-08-04 22:31:25 -0400

    Move Player Logic To Model

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -15,23 +15,6 @@ class Api::V1::PlayersController < ApplicationController
         render json: player
     end
 
-    def turn
-        player = Player.find_by(id: params[:id])
-        playerturn = true
-
-        #if player.draws
-            #player.get_card
-            #player.hand_value += card_value
-        #end
-
-        if player.hand_value > 21 || #player stands
-            playerturn = false
-        end
-
-        if playerturn == true
-            player.turn #calls method again
-        end
-    end
 
     private
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,20 @@
 class Player < ApplicationRecord
     
+    def turn
+        player = Player.find_by(id: params[:id])
+        playerturn = true
 
+        #if player.draws
+            #player.get_card
+            #player.hand_value += card_value
+        #end
+
+        if player.hand_value > 21 || #player stands
+            playerturn = false
+        end
+
+        if playerturn == true
+            player.turn #calls method again
+        end
+    end
 end
