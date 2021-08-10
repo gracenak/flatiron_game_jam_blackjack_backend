7934d57f8be20a560412a3ddc56c013ad086a2c1
Sean <Sransonette@gmail.com>
2021-08-09 19:29:57 -0500

    create a cards controller and delete decks controller and model

---
+++ app/controllers/api/v1/cards_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::DecksController < ApplicationController
+
+    def index 
+        @cards = Cards.all
+        render json: @cards
+
+    end
+
+    def show
+        @cards = Cards.find_by(id: params[:id])
+
+    end
+
+    
+
+    private
+
+    def deck_params
+        params.require(:card).permit(:value, :suit)
+    end
+
+end
